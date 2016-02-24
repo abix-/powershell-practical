a7e66d0970bbace31b184d8f214eac6288b19264
Al Iannacone <a.aiannacone@gmail.com>
2016-02-24 18:10:31 -0500

    Revert "i love powershell"
    This reverts commit b5e4fe56fa37089ed31e1f7f710bec65bb93da21.

--- vmware/Enable-SSH.ps1
+++
@@ -1,5 +0,0 @@
-﻿$vmhosts = get-content hosts_ord.txt
-foreach($vmhost in $vmhosts) {
-    Write-Host "Working on $vmhost"
-    Get-VMHost $vmhost | Get-VMHostService | Where {$_.Key –eq “TSM-SSH”} | Start-VMHostService
-}
